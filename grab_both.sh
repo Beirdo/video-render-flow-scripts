9d6cf2a2a760b6932e12f81cef1bcb55101c51f5
Gavin Hurlbut <gjhurlbu@gmail.com>
2017-04-13 00:10:58 -0700

    OK, have shared video now for grab_both

--- grab_both.sh
+++ grab_both.sh
@@ -6,6 +6,12 @@ if [ "$1" == "--headset" ]; then
     shift 1
 fi
 
+BUILTIN=
+if [ "$1" == "--builtin" ]; then
+    BUILTIN="1"
+    shift 1
+fi
+
 SCOPEVIDDEVICE=$(find_video_dev.py "USB2.0 Camera")
 if [ $? -ne 0 ]; then
     exit 1
@@ -21,11 +27,13 @@ fi
 #AUDDEVICE="alsa_input.usb-Etron_Technology__Inc._USB2.0_Camera-02.analog-mono"
 # Use the laptop's builtin mic
 
-CAMAUDDEVICE="alsa_input.usb-046d_HD_Pro_Webcam_C920_4BB47EAF-02.analog-stereo"
+AUDDEVICE="alsa_input.usb-046d_HD_Pro_Webcam_C920_4BB47EAF-02.analog-stereo"
 if [ -n "$HEADSET" ]; then
-    CAMAUDDEVICE="alsa_input.usb-Logitech_Inc_Logitech_USB_Headset_H540_00000000-00.analog-stereo"
+    AUDDEVICE="alsa_input.usb-Logitech_Inc_Logitech_USB_Headset_H540_00000000-00.analog-stereo"
 fi
-SCOPEAUDDEVICE="alsa_input.pci-0000_00_1b.0.analog-stereo"
+if [ -n "$BUILTIN" ]; then
+    AUDDEVICE="alsa_input.pci-0000_00_1b.0.analog-stereo"
+fi
 
 OUTDIR=/opt/video/render/rawinput
 SCOPEFILENAME=microscope-$(date +%F-%T).mkv
@@ -48,24 +56,23 @@ gst-launch-1.0 -ve \
     v4l2src device="$SCOPEVIDDEVICE" typefind=true ! \
         video/x-raw,format=YUY2,framerate=30/1,width=640,height=480 ! \
         tee name=vid1 \
-    pulsesrc device="$SCOPEAUDDEVICE" ! \
-        audio/x-raw,format=S16LE,rate=48000,channels=2 ! identity sync=true ! \
-        queue name=audioq1 \
+    pulsesrc device="$AUDDEVICE" ! \
+        audio/x-raw,format=S16LE,rate=48000,channels=2 ! \
+        tee name=audiotee \
     vid1. ! queue ! videoconvert ! identity sync=true ! \
         x264enc bitrate=3000 speed-preset=ultrafast ! queue name=videoq1 \
     videoq1. ! matroskamux name=mux1 ! filesink location=${OUTDIR}/${SCOPEFILENAME} \
-    audioq1. ! mux1. \
+    audiotee. ! identity sync=true ! \
+        queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! mux1. \
     v4l2src device="$CAMVIDDEVICE" ! \
         video/x-h264 ! h264parse ! \
         tee name=vid2 \
-    pulsesrc device="$CAMAUDDEVICE" ! \
-        audio/x-raw,format=S16LE,rate=48000,channels=2 ! identity sync=true ! \
-        queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 name=audioq2 \
     vid2. ! queue ! \
         video/x-h264,width=1920,height=1080,framerate=30/1,stream-format=avc ! \
         identity sync=true ! queue name=videoq2 \
     videoq2. ! mux2. \
-    audioq2. ! mux2. \
+    audiotee. ! identity sync=true ! \
+        queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! mux2. \
     matroskamux streamable=true name=mux2 ! \
         filesink location=${OUTDIR}/${CAMFILENAME} \
     vid1. ! queue ! videorate ! video/x-raw,framerate=$VIEWRATE ! \
@@ -85,4 +92,6 @@ gst-launch-1.0 -ve \
         x264enc bitrate=2000 speed-preset=ultrafast ! queue name=videoq3 \
     videoq3. ! matroskamux name=mux3 ! \
 	filesink location=${OUTDIR}/${PREVIEWFILENAME} \
+    audiotee. ! identity sync=true ! \
+        queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! mux3. \
 
