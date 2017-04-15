570ed03b80e507bcef4bb2d9a285e3da24ab290e
Gavin Hurlbut <gjhurlbu@gmail.com>
2017-04-14 23:38:48 -0700

    Added render_pitivi.sh, remove ffmpeg conversion

---
+++ render_pitivi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+XGES_FILE=$1
+OUT_FILE=$2
+DEBUG=0
+
+export GST_PLUGIN_PATH=/usr/local/lib/gstreamer-1.0:/usr/lib/gstreamer-1.0
+
+ges-launch-1.0 --gst-debug-level ${DEBUG} -l ${XGES_FILE} -o ${OUT_FILE} -f "video/quicktime,variant=iso:video/x-raw,width=1920,height=1080->video/x-h264,speed-preset=ultrafast,bitrate=9000:audio/mpeg,mpegversion=4,rate=48000"
