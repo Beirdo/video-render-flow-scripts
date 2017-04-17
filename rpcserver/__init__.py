93ce13dfcb41e8c7157f9ef485a647a07740e83f
Gavin Hurlbut <gjhurlbu@gmail.com>
2017-04-16 18:30:23 -0700

    Try to use a OutputHandler

--- rpcserver/__init__.py
+++ rpcserver/__init__.py
@@ -44,19 +44,52 @@ queueMap = {
 
 handlerThreads = {}
 handlers = {}
+outputThread = None
 
-def execCommand(command):
-    if not isinstance(command, list):
-        command = command.split()
-    try:
-        logger.info("Running %s" % " ".join(command))
-        return check_output(command, shell=False, stderr=STDOUT).decode("utf-8")
-    except CalledProcessError as e:
-        message = "Command: %s returned %s" % (e.cmd, e.returncode)
-        if e.output:
-            message += "\n\nOutput: %s" % e.output.decode("utf-8")
-        logger.error(message)
-        raise Exception(message)
+
+class OutputThread(Thread)
+    def __init__(self):
+        Thread.__init__(self)
+
+        self.sel = selectors.DefaultSelector()
+        self.idMap = {}
+        self.name = "OutputThread"
+        self.daemon = True
+        global handlers
+        self.handlers = handlers
+
+    def run(self):
+        while True:
+            events = sel.select()
+            for (key, mask) in events:
+                callback = key.data
+                callback(key.fileobj, mask)
+
+    def add(self, id_):
+        fd = self.handlers[id_]['pipe']
+        self.idMap[fd] = id_
+        self.sel.register(fd, selectors.EVENT_READ, self.read)
+
+    def removeId(self, id_):
+        fd = self.handlers[id_]['pipe']
+        self.remove(fd)
+
+    def remove(self, fd):
+        del self.idMap[fd]
+        self.sel.unregister(fd)
+        fd.close()
+
+    def read(self, fd, mask):
+        data = fd.read(1024)
+        if data:
+            id_ = self.idMap.get(fd, None)
+            handler = self.handlers.get(id_, None)
+            if id_ and handler:
+                handler['data'].append(data)
+                return
+
+        self.remove(fd)
+
 
 def get_remote_ip(remoteIP=None):
     logger.info("Remote IP: %s" % remoteIP)
@@ -72,15 +105,15 @@ def get_remote_ip(remoteIP=None):
 
 
 class HandlerThread(Thread):
-    def __init__(self, queue, queueName):
+    def __init__(self, queue, queueName, handlers):
         Thread.__init__(self)
 
         self.queue = queue
         self.name = "%s-handler" % queueName
+        self.daemon = True
+        self.handlers = handlers
 
     def run(self):
-        global handlers
-
         while True:
             item = self.queue.get()
 
@@ -93,33 +126,73 @@ class HandlerThread(Thread):
             if not id_ or not method:
                 continue
 
-            if id_ not in handlers:
-                handlers[id_] = {}
-            data = handlers[id_]
+            if id_ not in self.handlers:
+                self.handlers[id_] = {}
+            data = self.handlers[id_]
             data['status'] = 'in-progress'
+            data['processTime'] = time.time()
+            data['queueTime'] = time.time() - data['queueTime']
+            data['data'] = []
             logger.info("Starting handler for method %s (id %s)" %
                         (method, id_))
+            args['myId'] = id_
             try:
                 if not hasattr(self, method):
                     raise Exception("No thread handler exists for method %s" %
                                     method)
                 methodFunc = getattr(self, method)
-                data['result'] = methodFunc(**args)
+                output = methodFunc(**args)
+                if output:
+                    data['results'] = output
             except Exception as e:
                 data['error'] = str(e)
 
             data['status'] = "complete"
+            data['processTime'] = time.time() - data['processTime']
             logger.info("Finishing handler for method %s (id %s)" %
                         (method, id_))
 
-    def upload_inputs(self, project, remoteIP=None, force=False):
+    def execCommand(self, command, id_):
+        global outputThread
+        if not isinstance(command, list):
+            command = command.split()
+
+        logger.info("Running %s" % " ".join(command))
+        if not outputThread or not outputThread.is_alive():
+            outputThread = OutputThread()
+            outputThread.start()
+
+        retCode = -1
+        handler = self.handlers[id_]
+        with Popen(command, shell=False, stdin=None, stdout=PIPE,
+                   stderr=STDOUT, bufsize=0) as p:
+            handler['pipe'] = p.stdout
+            outputThread.add(id_)
+            p.wait()
+            retCode = p.returncode
+        finally:
+            outputThread.removeId(id_)
+
+        if handler['data']:
+            output = b" ".join(handler['data']).decode("utf-8")
+
+        if retCode:
+            message = "Command: %s returned %s" % (" ".join(command), retCode)
+            if output:
+                message += "\n\nOutput: %s" % output
+            logger.error(message)
+            raise Exception(message)
+
+        handler['result'] = output
+
+    def upload_inputs(self, myId, project, remoteIP=None, force=False):
         path = os.path.join("/opt/video/render/video", project, "input", "")
         command = ["rsync", "-avt", "%s:%s" % (remoteIP, path), path]
         if force:
             command.insert(2, "--delete")
-        return execCommand(command)
+        self.execCommand(command, myId)
 
-    def convert_inputs(self, project, files=None, factor=0.5):
+    def convert_inputs(self, myId, project, files=None, factor=0.5):
         path = os.path.join("/opt/video/render/video", project, "input", "")
         if not factor:
             factor = 0.5
@@ -132,48 +205,46 @@ class HandlerThread(Thread):
             files = [os.path.join(path, file_) for file_ in files]
 
         # Dedupe and check existing files
+        r
         files = [file_ for file_ in set(files) if os.path.exists(file_)]
 
         if not files:
             return "No files in project %s" % project
 
-
-        output = ""
         for file_ in files:
             command = ["convert_gstream.sh", "--factor", str(factor), file_]
-            output += "\n\n"
-            output += execCommand(command)
+            self.handlers[myId]['output'].append("\n\n")
+            self.execCommand(command, myId)
 
-        return output
-
-    def download_proxies(self, project, remoteIP=None, force=False):
+    def download_proxies(self, myId, project, remoteIP=None, force=False):
         path = os.path.join("/opt/video/render/video", project, "proxy", "")
         command = ["rsync", "-avt", path, "%s:%s" % (remoteIP, path)]
         if force:
             command.insert(2, "--delete")
-        return execCommand(command)
+        self.execCommand(command, myId)
 
-    def download_editables(self, project, remoteIP=None, force=False):
+    def download_editables(self, myId, project, remoteIP=None, force=False):
         path = os.path.join("/opt/video/render/video", project, "edit", "")
         command = ["rsync", "-avt", path, "%s:%s" % (remoteIP, path)]
         if force:
             command.insert(2, "--delete")
-        return execCommand(command)
+        self.execCommand(command, myId)
 
-    def upload_edl(self, project, edlfile="edl.xges", remoteIP=None):
+    def upload_edl(self, myId, project, edlfile="edl.xges", remoteIP=None):
         path = os.path.join("/opt/video/render/video", project, "edit",
                             edlfile)
         command = ["rsync", "-avt", "%s:%s" % (remoteIP, path), path]
-        return execCommand(command)
+        self.execCommand(command, myId)
 
-    def upload_proxy_edl(self, project, edlfile="edl.xges", remoteIP=None):
+    def upload_proxy_edl(self, myId, project, edlfile="edl.xges",
+                         remoteIP=None):
         path = os.path.join("/opt/video/render/video", project, "proxy",
                             edlfile)
         command = ["rsync", "-avt", "%s:%s" % (remoteIP, path), path]
-        return execCommand(command)
+        self.execCommand(command, myId)
 
-    def render_edl(self, project, edlfile="edl.xges", outfile="output.mp4",
-                   proxy=False, mode="pitivi"):
+    def render_edl(self, myId, project, edlfile="edl.xges",
+                   outfile="output.mp4", proxy=False, mode="pitivi"):
         if proxy:
             path = os.path.join("/opt/video/render/video", project, "proxy",
                                 "factor.txt")
@@ -208,7 +279,7 @@ class HandlerThread(Thread):
                 command = ["cinelerra-proxychange.py", edlfile, "-f",
                            "%s/(.*)$" % proxypath, "-t", "%s/\\1" % editpath,
                            "-s", str(factor), "-v", "-a"]
-                output += execCommand(command)
+                self.execCommand(command, myId)
 
             # Create the batchfile
             soup = BeautifulSoup("", "xml")
@@ -257,14 +328,12 @@ class HandlerThread(Thread):
 
             # Run the batch
             command = ["cin", "-r", batchfile]
-            output += execCommand(command)
+            self.execCommand(command, myId)
         elif mode == 'pitivi':
             command = ["render_pitivi.sh", edlfile, outputfile]
-            output += execCommand(command)
+            self.execCommand(command, myId)
 
-        return output
-
-    def upload_to_youtube(self, project, outfile="output.mp4", params={}):
+    def upload_to_youtube(self, myId, project, outfile="output.mp4", params={}):
         path = os.path.join("/opt/video/render/video", project, "output",
                             outfile)
         command = ["upload_video.py", "--file", path,
@@ -273,10 +342,10 @@ class HandlerThread(Thread):
                    "--category", str(int(params.get('category', 28))),
                    "--keywords", params.get('keywords', "None"),
                    "--privacyStatus", "public", "--noauth_local_webserver"]
-        return execCommand(command)
+        self.execCommand(command, myId)
 
-    def archive_to_s3(self, project, skip=False, inputs=False, delete=False,
-                      accelerate=False):
+    def archive_to_s3(self, myId, project, skip=False, inputs=False,
+                      delete=False, accelerate=False):
         command = ["archive_to_s3.py", "--project", project]
         if skip:
             command.append("--skip")
@@ -286,7 +355,7 @@ class HandlerThread(Thread):
             command.append("--delete")
         if accelerate:
             command.append("--accelerate")
-        return execCommand(command)
+        self.execCommand(command, myId)
 
 
 def launch_thread(method, args):
@@ -306,17 +375,19 @@ def launch_thread(method, args):
         queueName = "local"
         queue = queues['local']
 
+    global handlers
+    handlers[id_] = {
+        "status": "queued",
+        "queueTime": time.time(),
+    }
+
     logger.info("Queuing request for %s method %s (id %s)" %
                 (queueName, method, id_))
     queue.put(data, block=False)
 
-    global handlers
-    handlers[id_] = {"status": "queued"}
-
     global handlerThreads
     if not handlerThreads.get(queueName, None):
-        handlerThreads[queueName] = HandlerThread(queue, queueName)
-        handlerThreads[queueName].daemon = True
+        handlerThreads[queueName] = HandlerThread(queue, queueName, handlers)
         handlerThreads[queueName].start()
 
     return "Please poll with id %s" % id_
@@ -458,13 +529,19 @@ def poll(id):
 
     handler = handlers[id]
     status = handler['status']
-    if status == "complete":
-        del handlers[id]
-        if "result" in handler:
-            return handler['result']
-        raise Exception(handler.get('error', "Unknown error"))
+    result = {
+        "status": status,
+        "result": b"".join(handler['data']).decode("utf-8"),
+        "queueDuration": handler['queueTime'],
+    }
 
-    return "status: %s" % status
+    if status == "complete":
+        result["processDuration"] = time.time() - handler['processTime']
+        del handlers[id]
+        if not handler.get('data', None):
+            raise Exception(handler.get('error', "Unknown error"))
+
+    return json.dumps(result)
 
 @jsonrpc.method("App.list_outstanding() -> Array", validate=True)
 def list_outstanding():
